041956b0bba9bf534e9c4a9b65589b0411dadc54
Vira Voronova <vera.voronova@hotmail.com>
2023-02-16 12:12:45 +0100

    basebane changed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pet-support-front/">
+    <BrowserRouter basename="/pet-support-front">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
