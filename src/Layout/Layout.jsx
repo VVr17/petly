9c8098c6f3c7492a3645da4abbe5372d45914550
Vira Voronova <vera.voronova@hotmail.com>
2023-02-16 12:07:31 +0100

    outlet added

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-  return <div>layout</div>;
+  return (
+    <div>
+      layout
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 };
 export default Layout;
