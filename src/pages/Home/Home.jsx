a4c553879190790610abcb4cb8fb312f8d6af82d
Vira Voronova <vera.voronova@hotmail.com>
2023-02-16 12:35:28 +0100

    router made

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>
-      Home
-      <Link to="/login">login</Link>
-      <Link to="/register">register</Link>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
