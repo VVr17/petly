e34790b2b92d9d027036b9dbab665ecd7f1a8d2a
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-02-17 11:53:23 +0100

    added proptypes

--- src/components/Ui-Kit/Input/Input.jsx
+++ src/components/Ui-Kit/Input/Input.jsx
@@ -1,15 +1,24 @@
-
 import React from "react";
 import { Field, ErrorMessage } from "formik";
+import PropTypes from 'prop-types';
 
-const InputField = ({ label, name, type, placeholder }) => {
+const InputField = ({ label, name, type, placeholder, id }) => {
   return (
     <>
       <label htmlFor={name}>{label}</label>
-      <Field type={type} name={name} id={name} placeholder={placeholder} />
+      <Field type={type} name={name} id={id} placeholder={placeholder} />
       <ErrorMessage name={name} component="div" />
     </>
   );
 };
 
 export default InputField;
+
+
+InputField.propTypes = {
+  label: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string,
+};
