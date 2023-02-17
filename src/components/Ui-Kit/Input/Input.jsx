ad2246747d164d199e0fca123b0081a83e0921b7
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-02-17 11:36:40 +0100

    reusable input

--- src/components/Ui-Kit/Input/Input.jsx
+++ src/components/Ui-Kit/Input/Input.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
 
-const Input = () => {
-  return <div>Input</div>;
+import React from "react";
+import { Field, ErrorMessage } from "formik";
+
+const InputField = ({ label, name, type, placeholder }) => {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <Field type={type} name={name} id={name} placeholder={placeholder} />
+      <ErrorMessage name={name} component="div" />
+    </>
+  );
 };
 
-export default Input;
+export default InputField;
