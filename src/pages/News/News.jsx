b8e61e973e391bb446c0dfd35b24f911003d5296
KsanaSal <oasalivon@gmail.com>
2023-02-18 19:33:18 +0200

    Added search form

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,19 +1,15 @@
 import NewsCard from 'components/NewsCard/NewsCard';
 import Section from 'components/Section';
+import SearchForm from 'components/Ui-Kit/SearchForm/SearchForm';
 import React from 'react';
-import { marginBottom } from 'styled-system';
 import { Title, WrapperTitle } from './News.styled';
-
-const styles = {
-  search: { backgroundColor: 'red', width: 280, height: 48 },
-};
 
 const News = () => {
   return (
     <Section>
       <WrapperTitle>
         <Title>News</Title>
-        <div style={styles.search}></div>
+        <SearchForm></SearchForm>
       </WrapperTitle>
 
       <NewsCard />
