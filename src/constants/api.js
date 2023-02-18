73810ea01c5466904877777ab3645105b6a06365
Vira Voronova <vera.voronova@hotmail.com>
2023-02-18 11:54:27 +0100

    persist added

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://pet-support.up.railway.app';
+export const BASE_URL = 'https://pet-support.up.railway.app/app';
+export const USER_URL = '/auth';
 
 export const TAGS_TYPES = {
   user: 'User',
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userApi } from './api/userApi';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(userApi.middleware),
 });
+
+export const persistor = persistStore(store);
