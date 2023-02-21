cea1c108a414ee927fb42958b33d0403d6ca9acd
KsanaSal <oasalivon@gmail.com>
2023-02-21 19:15:16 +0200

    Change background

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,22 @@ import { Box } from 'components/Box/Box';
 import Container from 'components/Container';
 import { pageAnimation } from 'constants/animation';
 import React from 'react';
-import { Title, WrapContainer } from './Home.styled';
+import { Background, Images, Title, WrapContainer } from './Home.styled';
+import portraitMobile from 'assets/images/mobile/portrait-and-favorite-pet.png';
+import portraitTablet from 'assets/images/tablet/portrait-and-favorite-pet.png';
+import portraitDesktop from 'assets/images/desktop/portrait-and-favorite-pet.png';
 
 const Home = () => {
   return (
-    <WrapContainer {...pageAnimation} transition={{ duration: 0.3 }}>
-      <Title>
-        Take good care of <br />
-        your small pets
-      </Title>
-    </WrapContainer>
+    <Background>
+      <WrapContainer {...pageAnimation} transition={{ duration: 0.3 }}>
+        <Title>
+          Take good care of <br />
+          your small pets
+        </Title>
+        <Images src={portraitMobile} />
+      </WrapContainer>
+    </Background>
   );
 };
 
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,8 +1,8 @@
 import { theme } from 'constants/theme';
 import styled from 'styled-components';
-import waveMobile from 'assets/images/mobile/wave.svg';
-import waveTablet from 'assets/images/tablet/wave.svg';
-import waveDesktop from 'assets/images/desktop/wave-bubbel-heart.svg';
+import waveMobile from 'assets/images/mobile/wave.png';
+import waveTablet from 'assets/images/tablet/wave.png';
+import waveDesktop from 'assets/images/desktop/wave-bubbel-heart.png';
 import portraitMobile from 'assets/images/mobile/portrait-and-favorite-pet.png';
 import portraitTablet from 'assets/images/tablet/portrait-and-favorite-pet.png';
 import portraitDesktop from 'assets/images/desktop/portrait-and-favorite-pet.png';
@@ -29,16 +29,16 @@ export const WrapContainer = styled(motion.div)`
   padding-left: 20px;
   width: ${theme.breakpoints[0]};
   min-height: 532px;
-  background: url(${portraitMobile}) no-repeat bottom 0 left 0,
-    url(${waveMobile}) no-repeat bottom 0 left 0;
+  /* background: url(${waveMobile}) no-repeat bottom 0 left 0; */
+  /* background-size: 100% 456px; */
+  /* position: relative; */
 
   ${theme.mq.tablet} {
     width: ${theme.breakpoints[1]};
     min-height: 1127px;
     padding-right: ${({ theme }) => theme.space[6]};
     padding-left: ${({ theme }) => theme.space[6]};
-    background: url(${portraitTablet}) no-repeat bottom 0 left 60px,
-      url(${waveTablet}) no-repeat bottom 0 left 0px;
+    /* background: url(${waveTablet}) no-repeat bottom 0 left 0px; */
   }
 
   ${theme.mq.desktop} {
@@ -46,7 +46,30 @@ export const WrapContainer = styled(motion.div)`
     padding-right: ${({ theme }) => theme.space[3]};
     padding-left: ${({ theme }) => theme.space[3]};
     min-height: 702px;
-    background: url(${portraitDesktop}) no-repeat bottom 0 left 736px,
-      url(${waveDesktop}) no-repeat bottom 0 left 62px;
+    /* background: url(${waveDesktop}) no-repeat bottom 0 left 62px; */
+  }
+`;
+
+export const Images = styled.img`
+  margin-top: 58px;
+  width: 320px;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+export const Background = styled.div`
+  background: bottom 0 left 0 / contain no-repeat url(${waveMobile});
+  background-size: 100% 456px;
+  width: 100%;
+
+  ${theme.mq.tablet} {
+    background: bottom 0 left 0 / contain no-repeat url(${waveTablet});
+    background-size: 100% 1079px;
+  }
+
+  ${theme.mq.desktop} {
+    background: bottom 0 left 62px / contain no-repeat url(${waveDesktop});
+    background-size: 100% 598px;
   }
 `;
