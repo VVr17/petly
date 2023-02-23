5b954646fe8b640363e1e90aad236c557b610cdc
Vira Voronova <vera.voronova@hotmail.com>
2023-02-23 09:34:52 +0100

    merged

--- src/redux/api/petsApi.js
+++ src/redux/api/petsApi.js
@@ -11,9 +11,6 @@ export const petsApi = createApi({
       query: newPetData => ({
         url: `${PETS_URL}`,
         method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
         body: newPetData,
       }),
       invalidatesTags: [TAGS_TYPES.pet],
