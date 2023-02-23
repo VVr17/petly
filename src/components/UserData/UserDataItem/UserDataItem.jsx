be530bf46d1c20691fec5198bdf131e9eab253d9
Nick Zheliba <nikolaj.jeliba@gmail.com>
2023-02-23 11:11:06 +0200

    fix code

--- src/components/UserData/UserDataItem/UserDataItem.jsx
+++ src/components/UserData/UserDataItem/UserDataItem.jsx
@@ -3,36 +3,36 @@ import { Label, Input, Button } from "./UserDataItem.styled";
 import { Pencil } from "./UserDataItem.styled";
 
 export const UserDataItem = () => {
-    return( <>
-        <Label>Name:
-        <Input name="name" type="name"/>
-        <Button type="submit">
-        <Pencil/>
-        </Button>
-        </Label>
-        <Label>Email:
-        <Input name="email" type="email"/>
-        <Button type="submit">
-        <Pencil/>
-        </Button>
-        </Label>
-        <Label>Birthday:
-        <Input name="birthday" type="birthday"/>
-        <Button type="submit">
-        <Pencil/>
-        </Button>
-        </Label>
-        <Label>Phone:
-        <Input name="phone" type="phone"/>
-        <Button type="submit">
-        <Pencil/>
-        </Button>
-        </Label>
-        <Label>City:
-        <Input name="city" type="city"/>
-        <Button type="submit">
-        <Pencil/>
-        </Button>
-        </Label> 
-    </>
-    )}
+  return( <>
+  <Label>Name:
+    <Input name="name" type="name"/>
+    <Button type="submit">
+      <Pencil/>
+    </Button>
+  </Label>
+  <Label>Email:
+    <Input name="email" type="email"/>
+    <Button type="submit">
+      <Pencil/>
+    </Button>
+  </Label>
+  <Label>Birthday:
+    <Input name="birthday" type="birthday"/>
+    <Button type="submit">
+      <Pencil/>
+    </Button>
+  </Label>
+  <Label>Phone:
+    <Input name="phone" type="phone"/>
+    <Button type="submit">
+      <Pencil/>
+    </Button>
+  </Label>
+  <Label>City:
+    <Input name="city" type="city"/>
+    <Button type="submit">
+      <Pencil/>
+    </Button>
+  </Label> 
+</>  
+  )}
