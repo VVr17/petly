e25a0c19495a71ca2b359c6639f7c64300c6443f
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2023-02-23 22:43:15 +0200

    add backdrop to tablet menu

--- src/components/MobMenu/MobMenu.jsx
+++ src/components/MobMenu/MobMenu.jsx
@@ -7,34 +7,51 @@ import {
   Button,
   Link,
   Span,
+  Overlay,
 } from './MobMenu.styled';
 import PropTypes from 'prop-types';
 import { VscChromeClose } from 'react-icons/vsc';
 import Nav from 'components/Nav/Nav';
 import AuthNav from 'components/AuthNav';
 import UserNav from 'components/UserNav';
-import { mobileMenuAnimation } from 'constants/animation';
+import { mobileMenuAnimation, pageAnimation } from 'constants/animation';
 
-const MobMenu = ({ closeMenu, isAuth , loginIsActive, closeMenuRegister}) => {
+const MobMenu = ({ closeMenu, isAuth, loginIsActive, closeMenuRegister }) => {
+  const backdropClickCloseMenu = e => {
+    if (e.currentTarget === e.target) {
+      closeMenu();
+    }
+  };
+
   return (
-    <Modal key="mobile" {...mobileMenuAnimation} transition={{ duration: 0.3 }}>
-      <Container>
-        <Link to="/" end onClick={closeMenu}>
-          pe<Span>t</Span>ly
-        </Link>
-        <Button onClick={closeMenu}>
-          <VscChromeClose style={iconStyle} />
-        </Button>
-      </Container>
-      <AuthBox>
-        {isAuth ? (
-          <UserNav closeMenu={closeMenu} />
-        ) : (
-            <AuthNav closeMenu={closeMenu} loginIsActive={loginIsActive} closeMenuRegister={closeMenuRegister} />
-        )}
-      </AuthBox>
-      <Nav closeMenu={closeMenu} />
-    </Modal>
+    <Overlay onClick={backdropClickCloseMenu} {...pageAnimation}>
+      <Modal
+        key="mobile"
+        {...mobileMenuAnimation}
+        transition={{ duration: 0.3 }}
+      >
+        <Container>
+          <Link to="/" end onClick={closeMenu}>
+            pe<Span>t</Span>ly
+          </Link>
+          <Button onClick={closeMenu}>
+            <VscChromeClose style={iconStyle} />
+          </Button>
+        </Container>
+        <AuthBox>
+          {isAuth ? (
+            <UserNav closeMenu={closeMenu} />
+          ) : (
+            <AuthNav
+              closeMenu={closeMenu}
+              loginIsActive={loginIsActive}
+              closeMenuRegister={closeMenuRegister}
+            />
+          )}
+        </AuthBox>
+        <Nav closeMenu={closeMenu} />
+      </Modal>
+    </Overlay>
   );
 };
 
@@ -42,7 +59,7 @@ MobMenu.propTypes = {
   closeMenu: PropTypes.func.isRequired,
   isAuth: PropTypes.bool.isRequired,
   loginIsActive: PropTypes.bool.isRequired,
-  closeMenuRegister: PropTypes.func.isRequired
+  closeMenuRegister: PropTypes.func.isRequired,
 };
 
 export default MobMenu;
