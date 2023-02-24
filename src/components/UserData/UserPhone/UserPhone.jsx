7944635cfd7f0d9052d8ad4f2ac0348a1041744e
Vira Voronova <vera.voronova@hotmail.com>
2023-02-24 13:35:37 +0100

    userform

--- src/components/UserData/UserPhone/UserPhone.jsx
+++ src/components/UserData/UserPhone/UserPhone.jsx
@@ -1,49 +1,53 @@
 import UserInput from 'components/Ui-Kit/UserInput';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
+import { useUpdateUserMutation } from 'redux/api/userApi';
 
 const UserPhone = () => {
   const [isDisabled, setIsDisabled] = useState(true);
+  const [updateUser] = useUpdateUserMutation();
 
-  const handleSubmit = () => {
+  const handleSubmit = value => {
     //handlesubmit
-
+    updateUser(value);
     setIsDisabled(true);
   };
 
-  <Formik
-    initialValues={initialValues}
-    // validationSchema={    }
-    onSubmit={handleSubmit}
-    // encType="multipart/form-data"
-  >
-    {({ isSubmitting, values, setFieldValue }) => (
-      <Form>
-        <div>
-          <UserInput
-            label="Phone"
-            name="phone"
-            type="phone"
-            isDisabled={isDisabled}
-          />
-          {isDisabled ? (
-            <button
-              type="button"
-              onClick={() => {
-                setIsDisabled(false);
-              }}
-            >
-              {/* <Pencil /> */}
-            </button>
-          ) : (
-            <button type="submit" onClick={handleSubmit}>
-              {/* <галочка /> */}
-            </button>
-          )}
-        </div>
-      </Form>
-    )}
-  </Formik>;
+  return (
+    <Formik
+      initialValues={initialValues}
+      // validationSchema={ }
+      onSubmit={handleSubmit}
+      // encType="multipart/form-data"
+    >
+      {({ isSubmitting, values, setFieldValue }) => (
+        <Form>
+          <div>
+            <UserInput
+              label="Phone"
+              name="phone"
+              type="phone"
+              isDisabled={isDisabled}
+            />
+            {isDisabled ? (
+              <button
+                type="button"
+                onClick={() => {
+                  setIsDisabled(false);
+                }}
+              >
+                {/* <Pencil /> */}
+              </button>
+            ) : (
+              <button type="submit" onClick={handleSubmit}>
+                {/* <галочка /> */}
+              </button>
+            )}
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default UserPhone;
