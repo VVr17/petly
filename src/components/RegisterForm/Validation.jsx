09fad824547f163fe07c08f271416c7d2b57ad67
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-02-25 21:42:18 +0100

    styled and regexp fixed

--- src/components/RegisterForm/Validation.jsx
+++ src/components/RegisterForm/Validation.jsx
@@ -46,8 +46,7 @@ export const validationSchemaStepTwo = Yup.object().shape({
     .required('Name is required'),
   city: Yup.string()
     .matches(
-      /^[a-zA-Zа-яА-Я]+(?:[-a-zA-Zа-яА-Я]+)*,\s*[a-zA-Zа-яА-Я'’\s-]+$/,
-
+      /^[a-zA-Zа-яА-Я]+(?:[-\s]?[a-zA-Zа-яА-Я]+)*,\s*[a-zA-Zа-яА-Я'’\s-]+$/,
       'Should be at least 2 words separated by comma'
     )
     .min(3, 'City should have at least 3 characters')
