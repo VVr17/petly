5f69624905f46576911b2876ff7e2e0e1b34fa3f
Vira Voronova <vera.voronova@hotmail.com>
2023-02-26 18:04:44 +0100

    fixed pets

--- src/components/UserPetsList/UserPetsList.jsx
+++ src/components/UserPetsList/UserPetsList.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useState } from 'react';
-import { Container, Title, Box, List, ImageBox, Image, ImageSpan } from './UserPetsList.styled';
+import {
+  Container,
+  Title,
+  Box,
+  List,
+  ImageBox,
+  Image,
+  ImageSpan,
+} from './UserPetsList.styled';
 import { useGetCurrentUserQuery } from 'redux/api/userApi';
 import UserAddPetForm from 'components/UserAddPetForm';
 import UserPetsListItem from './UserPetsListItem';
 import Modal from 'components/Modal';
 import { AnimatePresence } from 'framer-motion';
 import AddPetButton from 'components/AddPetButton';
-import Pets from '../../assets/images/desktop/pet.jpg'
+import Pets from '../../assets/images/desktop/pet.jpg';
 
 const UserPetsList = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,16 +37,18 @@ const UserPetsList = () => {
         <Title>My pets:</Title>
         <AddPetButton handleClick={handleIsOpen} />
       </Box>
-      {data.pets.length > 0 ? (
+      {data?.pets.length > 0 ? (
         <List>
           {data?.pets.map(pet => {
             return <UserPetsListItem key={pet._id} pet={pet} />;
           })}
         </List>
-      ) : <ImageBox>
-            <Title>You have not added your pets yet.</Title>
-            <Image src={Pets} alt='pets'></Image>
-          </ImageBox>}
+      ) : (
+        <ImageBox>
+          <Title>You have not added your pets yet.</Title>
+          <Image src={Pets} alt="pets"></Image>
+        </ImageBox>
+      )}
       <AnimatePresence>
         {isOpen && (
           <Modal closeModal={closeModal} key="popUp">
