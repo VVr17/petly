9c1ff0bebeac0971a9811e8f2f3a66512fa06e90
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-02-28 11:23:20 +0100

    citiesList in user page

--- src/components/User/UserData/UserDataItem/UserCity/UserCity.jsx
+++ src/components/User/UserData/UserDataItem/UserCity/UserCity.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Formik, Form } from 'formik';
 import { useUpdateUserMutation } from 'redux/api/userApi';
 import { validationSchema } from './validation';
@@ -10,12 +10,15 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { selectUserState } from 'redux/user/userSelectors';
 import { toast } from 'react-toastify';
+import cities from 'assets/files/uaCities.json';
+import { List, ListItem } from './UserCity.styled';
 
 const UserCity = () => {
   const [isDisabled, setIsDisabled] = useState(true);
   const [updateUser, { isLoading }] = useUpdateUserMutation();
   const user = useSelector(selectUserState);
   const initialValues = { city: user?.city || '' };
+  const [filteredCities, setFilteredCities] = useState([]);
 
   const handleClick = (values, actions) => {
     if (isDisabled) {
@@ -47,32 +50,60 @@ const UserCity = () => {
       validationSchema={validationSchema}
       onSubmit={handleSubmit}
     >
-      {({ isSubmitting, values, errors }) => (
-        <Form>
-          <FieldWrapper>
-            <UserInput
-              label="City"
-              name="city"
-              type="city"
-              disabled={isDisabled}
-              placeholder={user.city || ''}
-            />
-            <UserUpdateButton
-              type="submit"
-              isdisabled={isDisabled}
-              onClick={() => {
-                if (!values.city) {
-                  values.city = user.city;
+      {({ isSubmitting, values, errors, setFieldValue }) => {
+        useEffect(() => {
+          const inputValue = values.city || '';
+          if (inputValue) {
+            const filtered = cities
+              .filter((city) => city.city.toLowerCase().startsWith(inputValue.toLowerCase()))
+              .map(({ city, admin_name }) => `${city}, ${admin_name}`);
+            setFilteredCities(filtered);
+          } else {
+            setFilteredCities([]);
+          }
+        }, [values.city]);
+
+        const handleCityClick = (city, setFieldValue) => {
+          setFieldValue('city', city);
+          setFilteredCities([]);
+        };
+
+        return (
+          <Form>
+            <FieldWrapper>
+              <UserInput
+                label="City"
+                name="city"
+                type="city"
+                disabled={isDisabled}
+                placeholder={user.city || ''}
+              />
+              {filteredCities.length > 0 && (
+                <List>
+                  {filteredCities.map((city, index) => (
+                    <ListItem onClick={() => handleCityClick(city, setFieldValue)} key={index}>
+                      {city}
+                    </ListItem>
+                  ))}
+                </List>
+              )}
+              <UserUpdateButton
+                type="submit"
+                isdisabled={isDisabled}
+                onClick={() => {
+                  if (!values.city) {
+                    values.city = user.city;
+                    handleClick(values);
+                  }
+                  if (errors.city) return;
                   handleClick(values);
-                }
-                if (errors.city) return;
-                handleClick(values);
-              }}
-            />
-            {isLoading && <Loader />}
-          </FieldWrapper>
-        </Form>
-      )}
+                }}
+              />
+              {isLoading && <Loader />}
+            </FieldWrapper>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
@@ -82,3 +113,4 @@ UserCity.propTypes = {
 };
 
 export default UserCity;
+
